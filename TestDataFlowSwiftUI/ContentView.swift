9426457a3d8c5b7eb5b30c0dca4f8d2f42b8a48f
RafalSwat <rafal.swat@gmail.com>
2019-12-16 17:37:04 +0100

    Initial Commit

---
+++ TestDataFlowSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TestDataFlowSwiftUI
+//
+//  Created by Rafał Swat on 16/12/2019.
+//  Copyright © 2019 Rafał Swat. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
